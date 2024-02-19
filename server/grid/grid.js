9d75b00c438113aa653de1426fff5cc5388ff7ea
Adam D <adam@adamduda.com>
2024-02-18 18:26:10 -0600

    lifestone: comments & code for grid.js

--- server/grid/grid.js
+++ server/grid/grid.js
@@ -1,7 +1,7 @@
-//  N x N grid, row x column
+//  Grid Format:   [N x N]  [row x column]
 
 
-
+//  Method to [re]turn row from BigInt
 //  left 1/2 bits are row
 function reRow(BigInt=0){
 
@@ -9,7 +9,7 @@ function reRow(BigInt=0){
 }
 
 
-
+//  Method to [re]turn column from BigInt
 //  Right 1/2 bits are column
 function reCol(BigIn=0){
 
