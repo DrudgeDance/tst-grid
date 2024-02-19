fbf43a70d3bd17b4206b0cbe412f367a9c55db52
Adam D <adam@adamduda.com>
2024-02-18 18:46:43 -0600

    Lifestone: refactoring continued on reCol

--- server/grid/grid.js
+++ server/grid/grid.js
@@ -5,26 +5,30 @@
 //  left 1/2 bits are row
 function reRow(BigInt=0){
 
-
+    const leftHalf=0;
 }
 
 
 //  Method to [re]turn column from BigInt
 //  Right 1/2 bits are column
-function reCol(BigIn=0){
+function reCol(BigIn=0, debug = true){
 
-    let bin = 0b1111
-    const bitlength = bin.toString(2).length
-    const rightHalf=0;
-    const leftHalf=0;
-    bin+=2
-    let half = Math.floor(bitlength/2);
-    let mask = (1 << half) - 1
-    console.log(`Mask: ${mask.toString(2)}`)
-    console.log( (bin & mask) )
-
-    console.log(typeof bin, bin)
+    const rightHalf=0;                        // connect to Parameters later
     
+    const bin = 0b1111;                        // connect to Parameters later
+    const bitlength = bin.toString(2).length;
+    const half = Math.floor(bitlength/2);
+    const mask = (1 << half) - 1;
+    const column = BigInt(bin & mask);
+
+
+
+    if(debug === true){  
+        console.log(`DEBUG reCol: Original: ${bin.toString(2)}`)
+        console.log(`DEBUG reCol: theMask: ${mask.toString(2)}`)
+        console.log(`DEBUG reCol: appMask: ${column.toString(2)} `)
+        console.log(`DEBUG reCol: ${typeof bin}, ${bin}`)
+    }
 }
 
 
